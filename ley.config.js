0137f24788afac37bdfbfc8ef2d45075379b95a4
Karl Horky <karl.horky@gmail.com>
2024-03-29 14:49:52 +0100

    Enable Postgres.js snake to/from camel transform in Ley

--- ley.config.js
+++ ley.config.js
@@ -1,9 +1,11 @@
+import postgres from 'postgres';
 import { setEnvironmentVariables } from './util/config.js';
 
 setEnvironmentVariables();
 
 const options = {
   ssl: Boolean(process.env.POSTGRES_URL),
+  transform: postgres.camel,
 };
 
 export default options;
