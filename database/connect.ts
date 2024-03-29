3734bc3574cdc21c3664b976bd9ea2c79bd0d0cb
Karl Horky <karl.horky@gmail.com>
2024-03-29 15:09:16 +0100

    Add back missing `ssl` config

--- database/connect.ts
+++ database/connect.ts
@@ -14,6 +14,7 @@ declare module globalThis {
 function connectOneTimeToDatabase() {
   if (!('postgresSqlClient' in globalThis)) {
     globalThis.postgresSqlClient = postgres({
+      ssl: Boolean(process.env.POSTGRES_URL),
       transform: {
         ...postgres.camel,
         undefined: null,
