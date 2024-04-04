99ea339f9eac8fa8f62371e04020c2c5e5aa20a4
Karl Horky <karl.horky@gmail.com>
2024-04-04 21:03:55 +0200

    Extract Postgres.js config to util/config.js

--- ley.config.js
+++ ley.config.js
@@ -1,11 +1,5 @@
-import postgres from 'postgres';
-import { setEnvironmentVariables } from './util/config.js';
+import { postgresConfig, setEnvironmentVariables } from './util/config.js';
 
 setEnvironmentVariables();
 
-const options = {
-  ssl: Boolean(process.env.POSTGRES_URL),
-  transform: postgres.camel,
-};
-
-export default options;
+export default postgresConfig;
